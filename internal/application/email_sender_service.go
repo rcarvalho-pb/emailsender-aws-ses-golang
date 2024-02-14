73e69ccaec0b8337fddde053bc9f63706b10dd4e
Ramon Carvalho <rcarvalho.pb@gmail.com>
2024-02-14 10:53:27 -0300

    feat: service created

---
+++ internal/application/email_sender_service.go
@@ -0,0 +1,13 @@
+package application
+
+import (
+	"github.com/rcarvalho-pb/uber-email-sender-go/internal/adapters"
+)
+
+type EmailSenderUseCase struct {
+	EmailSenderGateway adapters.EmailSenderGateway
+}
+
+func (es *EmailSenderUseCase) SendEmail(toEmail, subject, body string) {
+	es.EmailSenderGateway.SendEmailGateway(toEmail, subject, body)
+}
